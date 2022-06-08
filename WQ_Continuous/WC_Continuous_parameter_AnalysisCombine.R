7d6bbb1d2d99982e7d568086173e9f5f3ece53ed
Joseph E Panzik <jepanzik@gmail.com>
2022-06-08 08:48:46 -0400

    Script to take SKT analysis for each parameter, import, combine, arrange by managed area, and write to file (with created files).

---
+++ WQ_Continuous/WC_Continuous_parameter_AnalysisCombine.R
@@ -0,0 +1,31 @@
+# This script is designed to read the file names for the Kendall Tau results,
+# import each one, combine into one data table, arrange by managed area, and write to file
+
+## WHEN RUNNING IN RSTUDIO:
+## Set working directory to "Source File Location" in "Session" menu
+
+
+library(data.table)
+library(dplyr)
+
+out_dir <- "output/by_parameter/Combined"
+
+#List all of the files in the "tables" directory that are KendallTau results
+files <- list.files("output/by_parameter", pattern="KendallTau", full.names=TRUE)
+
+for(i in 1:length(files)){
+      if(i==1){
+            data <- fread(files[i], sep="|", header=TRUE, stringsAsFactors=FALSE,
+                         na.strings="")
+      } else{
+            data <- bind_rows(data,
+                              fread(files[i], sep="|", header=TRUE,
+                                    stringsAsFactors=FALSE, na.strings=""))
+      }
+}
+data <- as.data.table(data[order(data$ManagedAreaName, data$ProgramID,
+                                 data$ProgramName, data$ProgramLocationID,
+                                 data$ParameterName), ])
+
+fwrite(data, paste0(out_dir,"/", "WQ_Continuous_All_KendallTau_Stats.txt"), sep="|")
+fwrite(data, paste0(out_dir,"/", "WQ_Continuous_All_KendallTau_Stats.csv"), sep=",")
---
+++ WQ_Discrete/WC_Discrete_parameter_AnalysisCombine.R
@@ -0,0 +1,30 @@
+# This script is designed to read the file names for the Kendall Tau results,
+# import each one, combine into one data table, arrange by managed area, and write to file
+
+## WHEN RUNNING IN RSTUDIO:
+## Set working directory to "Source File Location" in "Session" menu
+
+
+library(data.table)
+library(dplyr)
+
+out_dir <- "output/by_parameter/Combined"
+
+#List all of the files in the "tables" directory that are KendallTau results
+files <- list.files("output/by_parameter", pattern="KendallTau", full.names=TRUE)
+
+for(i in 1:length(files)){
+      if(i==1){
+            data <- fread(files[i], sep="|", header=TRUE, stringsAsFactors=FALSE,
+                         na.strings="")
+      } else{
+            data <- bind_rows(data,
+                              fread(files[i], sep="|", header=TRUE,
+                                    stringsAsFactors=FALSE, na.strings=""))
+      }
+}
+data <- as.data.table(data[order(data$ManagedAreaName, data$ParameterName,
+                                 data$RelativeDepth, data$ActivityType), ])
+
+fwrite(data, paste0(out_dir,"/", "WQ_Discrete_All_KendallTau_Stats.txt"), sep="|")
+fwrite(data, paste0(out_dir,"/", "WQ_Discrete_All_KendallTau_Stats.csv"), sep=",")
