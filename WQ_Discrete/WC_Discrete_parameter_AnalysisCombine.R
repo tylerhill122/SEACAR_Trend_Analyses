1d5694df17c6ae1cf59cd99216b31c16f60b2cca
Joseph E Panzik <jepanzik@gmail.com>
2022-10-19 14:07:36 -0400

    Updates of WQ discrete files after new data files (Part 1)

--- WQ_Discrete/WC_Discrete_parameter_AnalysisCombine.R
+++ WQ_Discrete/WC_Discrete_parameter_AnalysisCombine.R
@@ -1,5 +1,6 @@
 # This script is designed to read the file names for the Kendall Tau results,
 # import each one, combine into one data table, arrange by managed area, and write to file
+# Created by J.E. Panzik (jepanzik@usf.edu) for SEACAR
 
 ## WHEN RUNNING IN RSTUDIO:
 ## Set working directory to "Source File Location" in "Session" menu
@@ -12,6 +13,8 @@ out_dir <- "output/by_parameter/Combined"
 
 #List all of the files in the "tables" directory that are KendallTau results
 files <- list.files("output/by_parameter", pattern="KendallTau", full.names=TRUE)
+website <- fread("data/WebsiteParameters.csv",  sep=",", header=TRUE,
+                 stringsAsFactors=FALSE, na.strings="")
 
 for(i in 1:length(files)){
       if(i==1){
@@ -23,8 +26,13 @@ for(i in 1:length(files)){
                                     stringsAsFactors=FALSE, na.strings=""))
       }
 }
+
+data <- merge.data.frame(data, website, by=c("ParameterName","RelativeDepth",
+                                             "ActivityType"), all=TRUE)
+data$Website[is.na(data$Website)] <- 0
+data <- data %>%
+      select(AreaID, ManagedAreaName, everything())
 data <- as.data.table(data[order(data$ManagedAreaName, data$ParameterName,
                                  data$RelativeDepth, data$ActivityType), ])
-
 fwrite(data, paste0(out_dir,"/", "WQ_Discrete_All_KendallTau_Stats.txt"), sep="|")
 fwrite(data, paste0(out_dir,"/", "WQ_Discrete_All_KendallTau_Stats.csv"), sep=",")
--- WQ_Discrete/WC_Discrete_parameter_ReportCompile.R
+++ WQ_Discrete/WC_Discrete_parameter_ReportCompile.R
@@ -12,7 +12,9 @@ library(data.table)
 library(knitr)
 library(readr)
 library(dplyr)
+library(tictoc)
 
+tic()
 #Sets whether to run documents with plots or not (APP_Plots==TRUE to include plots)
 APP_Plots <- TRUE
 
@@ -26,6 +28,7 @@ suff_years <- 10
 #Easiest way to edit is to comment out undesired parameters.
 #If only one parameter is desired, comment out all other parameters and delete comma after remaining parameter
 all_params <- c(
+   "Chlorophyll_a_corrected_for_pheophytin",
    "Chlorophyll_a_uncorrected_for_pheophytin",
    "Colored_dissolved_organic_matter_CDOM",
    "Dissolved_Oxygen",
@@ -44,14 +47,16 @@ all_params <- c(
 #If only one depth is desired, comment out the other depth and delete comma after remaining depth
 all_depths <- c(
    "Surface",
-   "Bottom"
+   "Bottom",
+   "All"
 )
 
 #Sets the list of activity types to cycle through. This can be edited to limit the number of types.
 #If only one type is desired, comment out the other type and delete comma after remaining type
 all_activity <- c(
    "Field",
-   "Sample"
+   "Lab",
+   "All"
 )
 
 #Loads data file with list on managed area names and corresponding area IDs and short names
@@ -73,24 +78,40 @@ for (param_name in all_params){
    for (depth in all_depths){
       #Because secchi depth is does not have a bottom measurement, this statement skips
       #Secchi depth for bottom
-      if (param_name=="Secchi_Depth" & depth=="Bottom"){
+      if (param_name=="Secchi_Depth" & (depth=="Bottom" | depth=="All")){
          next
       }
       
       #Starts for loop that cycles through activity types.
       for (activity in all_activity){
-         #Skips Field loops for parameters that only have Sample measurements
-         if ((param_name=="Chlorophyll_a_uncorrected_for_pheophytin" |
-              param_name=="Total_Nitrogen" | param_name=="Total_Phosphorus" |
+         #Skips Field loops for parameters that only have Lab measurements
+         if ((param_name=="Chlorophyll_a_corrected_for_pheophytin" | 
+              param_name=="Chlorophyll_a_uncorrected_for_pheophytin" |
+              param_name=="Colored_dissolved_organic_matter_CDOM" |
+              param_name=="Total_Nitrogen" |
+              param_name=="Total_Phosphorus" |
               param_name=="Total_Suspended_Solids_TSS") & activity=="Field") {
             next
-         #Skips Sample loops for parameters that only have Field measurements
-         } else if ((param_name=="Colored_dissolved_organic_matter_CDOM" |
-                     param_name=="Dissolved_Oxygen" | param_name=="Dissolved_Oxygen_Saturation" |
-                     param_name=="pH" | param_name=="Secchi_Depth" |
-                     param_name=="Water_Temperature") & activity=="Sample") {
+         #Skips Lab loops for parameters that only have Field measurements
+         } else if ((param_name=="Dissolved_Oxygen" |
+                     param_name=="Dissolved_Oxygen_Saturation" |
+                     param_name=="pH" |
+                     param_name=="Secchi_Depth" |
+                     param_name=="Water_Temperature") & activity=="Lab") {
+            next
+         } else if ((param_name=="Chlorophyll_a_corrected_for_pheophytin" |
+                     param_name=="Colored_dissolved_organic_matter_CDOM" |
+                     param_name=="Dissolved_Oxygen" |
+                     param_name=="Dissolved_Oxygen_Saturation" |
+                     param_name=="pH" |
+                     param_name=="Secchi_Depth" |
+                     param_name=="Total_Nitrogen" |
+                     param_name=="Total_Phosphorus" |
+                     param_name=="Water_Temperature") & activity=="All") {
             next
          }
+         
+         
          
          #Renders WC_discrete_parameter.Rmd for each parameter combination and writes the report to a pdf
          #Stored in reports/by_parameter directory
@@ -109,3 +130,5 @@ for (param_name in all_params){
       }
    }
 }
+toc()
+writeLines(as.character(Sys.time()), "LatestFinishTime.txt")
