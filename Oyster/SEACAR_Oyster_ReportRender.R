38aa3eabf1d68473ae46b97784a4e664a8f45f1d
Joseph E Panzik <jepanzik@gmail.com>
2023-06-07 09:57:13 -0400

    Updated Oyster script for new data file, added MA summaries, changed directory structure, improved figure output. Analysis run on June 5, 2023 database export

---
+++ Oyster/SEACAR_Oyster_ReportRender.R
@@ -0,0 +1,81 @@
+#This script is created to automate the production of Rmd documents for oyster analysis.
+# Created by J.E. Panzik (jepanzik@usf.edu) for SEACAR
+
+## WHEN RUNNING IN RSTUDIO:
+## Set working directory to "Source File Location" in "Session" menu
+
+## To ensure this script runs smoothly, please run in a fresh session of R
+## Some other libraries cause this script not to work properly
+
+## THIS SCRIPT WILL ONLY RUN WITH brms version 2.16.3 or lower. DOES NOT WORK WITH brms 2.17.0
+
+#Load libraries
+library(knitr)
+library(readr)
+library(dplyr)
+library(data.table)
+
+#Sets whether to run documents with plots or not (APP_Plots==TRUE to include plots)
+Run_An <- TRUE
+
+#Set output directory
+out_dir <- "output"
+
+#Sets the list of parameter names to cycle through. This can be edited to limit the number of parameters.
+#Easiest way to edit is to comment out undesired parameters.
+#If only one parameter is desired, comment out all other parameters and delete comma after remaining parameter
+param_name <- "All_Oyster_Parameters"
+
+#Loads data file with list on managed area names and corresponding area IDs and short names
+MA_All <- fread("data/ManagedArea.csv", sep = ",", header = TRUE, stringsAsFactors = FALSE,
+                na.strings = "")
+
+
+#Gets the files with the file names containing the desired parameter
+file_in <- list.files("data", pattern=param_name, full=TRUE)
+
+if(param_name=="Hectares" & length(grep("All Parameters", file_in))>0){
+   file_in <- file_in[-grep("All Parameters", file_in)]
+}
+#Gets the specific file used and removes the directory names
+file_short <- sub("data/", "", file_in)
+
+#Renders SEACAR_CoastalWetlands.Rmd for each parameter combination and writes the
+#report to an html and Word document stored in reports/by_parameter directory
+file_out <-  paste0("SEACAR_", param_name)
+
+
+rmarkdown::render(input = "SEACAR_Oyster_Report.Rmd", 
+                  output_format = "pdf_document",
+                  output_file = paste0(file_out, ".pdf"),
+                  output_dir = out_dir,
+                  clean=TRUE)
+rmarkdown::render(input = paste0(out_dir, "/", file_out, ".md"),
+                  output_format = "word_document",
+                  output_file = paste0(file_out, ".docx"),
+                  output_dir = out_dir,
+                  clean=TRUE)
+
+#Removes unwanted files created in the rendering process
+unlink(paste0(out_dir, "/", file_out, ".md"))
+unlink(paste0(out_dir, "/", file_out, "_files"), recursive=TRUE)
+
+# Section to create zip files of all plots created
+# Gets list of all directories in output
+param_dirs <- list.dirs(out_dir, full.name=FALSE)
+# Only keeps Figure directories
+param_dirs <- param_dirs[ grepl("Figures", param_dirs) ]
+# Loops through each figure directory
+for(i in 1:length(param_dirs)){
+   # Makes full directory name
+   param <- paste0("output/",param_dirs[i])
+   # Creates shorter version of parameter directory to be used as zip file name
+   param_short <- gsub("_", "", param_dirs[i])
+   param_short <- gsub("/", "", param_dirs[i])
+   # Gets list of png plots in Figure directory
+   fig_list <- list.files(param, pattern=".png", full=TRUE)
+   # Sets name to be used for zip file
+   zip_name <- paste0("/Oyster", param_short)
+   # Zips png figures to a zip file with name created above
+   zip(paste0(param, zip_name), files=fig_list)
+}
---
+++ Oyster/SEACAR_Oyster_ResultsCompile.R
@@ -0,0 +1,163 @@
+#This script is designed to read the file names for the LME results of the BBpct analysis,
+#import each one, extract the intercept, slope, and p values, then write them to a pipe-delimited file
+library(data.table)
+library(dplyr)
+
+#List all of the files in the "tables" directory that are LME results
+file_list <- list.files("output", pattern="ModelResults", full.names=TRUE)
+
+#Include only those that are txt
+file_in <- file_list[grep("csv", file_list)]
+
+
+#Read in file
+data <- fread(file_in, sep = ",", header = TRUE, stringsAsFactors = FALSE, na.strings = "")
+
+############
+# #Include only those that are BBpct
+# file_in <- file_list[grep("csv", file_list)]
+# 
+# 
+# #Read in file
+# data <- fread(file_in, sep = ",", header = TRUE, stringsAsFactors = FALSE, na.strings = "")
+# 
+# data <- data[data$Converged==1,]
+##############
+
+#Keep only rows that are values with "fixed" in the effect column
+data <- data[data$effect=="fixed" & !is.na(data$effect),]
+
+#For each managed area and species, get the LME intercept, slope, and p values
+table <- data %>%
+      group_by(managed_area, indicator, live_date_qual, size_class, habitat_class) %>%
+      dplyr::summarise(Programs=unique(programs),
+                Intercept = estimate[term == "(Intercept)"],
+                ModelEstimate = estimate[term == "RelYear" |
+                                       term == "meRelYearSampleAge_StdevgrEQQuadIdentifier"],
+                StandardError = std.error[term == "RelYear" |
+                                                term == "meRelYearSampleAge_StdevgrEQQuadIdentifier"],
+                LowerConfidence = conf.low[term == "RelYear" |
+                                                  term == "meRelYearSampleAge_StdevgrEQQuadIdentifier"],
+                UpperConfidence = conf.high[term == "RelYear" |
+                                                 term == "meRelYearSampleAge_StdevgrEQQuadIdentifier"])
+
+#Change column names to better match other outputs
+setnames(table, c("managed_area", "indicator", "size_class", "live_date_qual", "habitat_class", "Programs"),
+         c("ManagedAreaName", "ParameterName", "SizeClass", "ShellType", "HabitatType", "ProgramNames"))
+
+table$ManagedAreaName[table$ManagedAreaName=="Fort Pickens Aquatic Preserve"] <-
+      "Fort Pickens State Park Aquatic Preserve"
+
+table$ManagedAreaName[table$ManagedAreaName=="St. Andrews Aquatic Preserve"] <-
+      "St. Andrews State Park Aquatic Preserve"
+
+table$ShellType[table$ShellType=="Exact"] <- "Live Oyster Shells"
+table$ShellType[table$ShellType=="Estimate"] <- "Dead Oyster Shells"
+
+table$ParameterName[table$ParameterName=="Size class"] <- "Shell Height"
+table$ParameterName[table$ParameterName=="Percent live"] <- "Percent Live"
+
+#Load data summaries
+# Gets list of all directories in output
+param_dirs <- list.dirs(out_dir, full.name=FALSE)
+# Only keeps root directory for parameters
+param_dirs <- param_dirs[ !grepl("Figures", param_dirs) ]
+param_dirs <- param_dirs[param_dirs!=""]
+# Loops through each parameter directory and gets list of Overall stats files
+for(i in 1:length(param_dirs)){
+  param <- paste0("output/",param_dirs[i])
+  if(i==1){
+    files<-list.files(param, pattern="Overall", full.name=TRUE)
+  } else{
+    temp <- list.files(param, pattern="Overall", full.name=TRUE)
+    files <- append(files, temp)
+  }
+}
+
+
+for(i in 1:length(files)){
+  if(i==1){
+    data_summ <- fread(files[i], sep="|", header=TRUE, stringsAsFactors=FALSE,
+                  na.strings="")
+  } else{
+    data_summ <- bind_rows(data_summ,
+                      fread(files[i], sep="|", header=TRUE,
+                            stringsAsFactors=FALSE, na.strings=""))
+  }
+}
+
+data_summ$ParameterName[data_summ$ParameterName=="ShellHeight_mm"] <- "Shell Height"
+data_summ$ParameterName[data_summ$ParameterName=="Density_m2"] <- "Density"
+data_summ$ParameterName[data_summ$ParameterName=="PercentLive_pct"] <- "Percent Live"
+
+
+table <- merge.data.frame(data_summ, table, by=c("ManagedAreaName", "ParameterName",
+                                                 "ShellType", "SizeClass", "HabitatType"),
+                             all=TRUE)
+
+table <- as.data.table(table[order(table$ManagedAreaName, table$ParameterName,
+                                   table$ShellType, table$SizeClass,
+                                   table$HabitatType), ])
+table <- table %>% select(AreaID, everything())
+
+#Write output table to a csv and pipe-delimited txt file
+fwrite(table, "output/Oyster_All_GLMM_Stats.txt", sep="|")
+fwrite(table, "output/Oyster_All_GLMM_Stats.csv", sep=",")
+
+###### Compile data used for plots
+
+#List all of the files in the "Tables" directory that are Shell Heights
+file_list <- list.files("data/GLMMs/AllDates/Data", pattern="_sh", full.names=TRUE)
+
+for(i in 1:length(file_list)){
+  if(i==1){
+    data <- readRDS(file_list[i])
+    data$ProgramID <- as.character(data$ProgramID)
+  } else{
+    temp_data <- readRDS(file_list[i])
+    temp_data$ProgramID <- as.character(temp_data$ProgramID)
+    data <- bind_rows(data, temp_data)
+  }
+}
+
+#Write output table to a csv and pipe-delimited txt file
+fwrite(table, "output/Shell_Height/Oyster_SH_plotdata.txt", sep="|")
+fwrite(table, "output/Shell_Height/Oyster_SH_plotdata.csv", sep=",")
+
+
+#List all of the files in the "tables" directory that are Density
+file_list <- list.files("data/GLMMs/AllDates/Data", pattern="_n_", full.names=TRUE)
+
+for(i in 1:length(file_list)){
+  if(i==1){
+    data <- readRDS(file_list[i])
+    data$ProgramID <- as.character(data$ProgramID)
+  } else{
+    temp_data <- readRDS(file_list[i])
+    temp_data$ProgramID <- as.character(temp_data$ProgramID)
+    data <- bind_rows(data, temp_data)
+  }
+}
+
+#Write output table to a csv and pipe-delimited txt file
+fwrite(table, "output/Density/Oyster_Den_plotdata.txt", sep="|")
+fwrite(table, "output/Density/Oyster_Den_plotdata.csv", sep=",")
+
+
+#List all of the files in the "tables" directory that are Density
+file_list <- list.files("data/GLMMs/AllDates/Data", pattern="_p_", full.names=TRUE)
+
+for(i in 1:length(file_list)){
+  if(i==1){
+    data <- readRDS(file_list[i])
+    data$ProgramID <- as.character(data$ProgramID)
+  } else{
+    temp_data <- readRDS(file_list[i])
+    temp_data$ProgramID <- as.character(temp_data$ProgramID)
+    data <- bind_rows(data, temp_data)
+  }
+}
+
+#Write output table to a csv and pipe-delimited txt file
+fwrite(table, "output/Percent_Live/Oyster_Pct_plotdata.txt", sep="|")
+fwrite(table, "output/Percent_Live/Oyster_Pct_plotdata.csv", sep=",")
