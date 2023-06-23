cacb88d531e22cf69b5d7fbb7c8c2dad27f0d90a
Joseph E Panzik <jepanzik@gmail.com>
2023-06-23 14:19:21 -0400

    Updated Nekton file names, added line to automatically set working directory, improved figure zipping, updated x-axis break logic

--- Nekton/Nekton_SpeciesRichness_ReportRender.R
+++ Nekton/Nekton_SpeciesRichness_ReportRender.R
@@ -12,9 +12,11 @@ library(readr)
 library(dplyr)
 library(data.table)
 library(utils)
+library(rstudioapi)
 
-#Sets whether to run documents with plots or not (APP_Plots==TRUE to include plots)
-APP_Plots <- TRUE
+# Gets directory of this script and sets it as the working directory
+wd <- dirname(getActiveDocumentContext()$path)
+setwd(wd)
 
 #Set output directory
 out_dir <- "output"
@@ -36,11 +38,11 @@ param_file <- "SpeciesRichness"
 #Gets the specific file used and removes the directory names
 file_short <- sub("data/", "", file_in)
 
-#Renders SEACAR_Nekton_SpeciesRichness.Rmd and writes the report to a pdf and 
+#Renders Nekton_SpeciesRichness.Rmd and writes the report to a pdf and 
 #Word document stored in output directory
-file_out <-  paste0("SEACAR_Nekton_", param_file)
+file_out <-  paste0("Nekton_", param_file, "_Report")
 
-rmarkdown::render(input = "SEACAR_Nekton_SpeciesRichness.Rmd", 
+rmarkdown::render(input = "Nekton_SpeciesRichness.Rmd", 
                   output_format = "pdf_document",
                   output_file = paste0(file_out, ".pdf"),
                   output_dir = out_dir,
@@ -57,5 +59,7 @@ unlink(paste0(out_dir, "/", file_out, "_files"), recursive=TRUE)
 
 
 #Gets list of all image files in output/Figures and creates zip directory
-fig_list <- list.files(paste0(out_dir, "/Figures"), pattern=".png", full=TRUE)
-zip(paste0(out_dir, "/Figures/NektonFigures"), files=fig_list)
+fig_list <- list.files(paste0(out_dir, "/Figures"), pattern=".png", full=FALSE)
+setwd(paste0(out_dir, "/Figures"))
+zip("NektonFigures", files=fig_list)
+setwd(wd)
