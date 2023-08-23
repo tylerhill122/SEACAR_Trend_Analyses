6c2f8a7ec96ef472c8b5f4c6f66f551bf0c2494a
tylerhill122 <tylergenehill122@gmail.com>
2023-08-23 15:56:53 -0400

    tested for all parameters at all depths

--- WQ_Discrete/WC_Discrete_data_table_export.R
+++ WQ_Discrete/WC_Discrete_data_table_export.R
@@ -5,40 +5,41 @@ library(ggplot2)
 library(ggpubr)
 library(tidyr)
 library(stringr)
+library(tictoc)
 
 all_params <- c(
-  # "Chlorophyll_a_corrected_for_pheophytin",
-  # "Chlorophyll_a_uncorrected_for_pheophytin",
-  # "Colored_dissolved_organic_matter_CDOM",
-  # "Dissolved_Oxygen",
-  # "Dissolved_Oxygen_Saturation",
-  # "pH",
-  # "Salinity",
-  # "Secchi_Depth",
-  # "Total_Nitrogen",
-  # "Total_Phosphorus",
-  # "Total_Suspended_Solids_TSS",
+  "Chlorophyll_a_corrected_for_pheophytin",
+  "Chlorophyll_a_uncorrected_for_pheophytin",
+  "Colored_dissolved_organic_matter_CDOM",
+  "Dissolved_Oxygen",
+  "Dissolved_Oxygen_Saturation",
+  "pH",
+  "Salinity",
+  "Secchi_Depth",
+  "Total_Nitrogen",
+  "Total_Phosphorus",
+  "Total_Suspended_Solids_TSS",
   "Turbidity",
   "Water_Temperature"
 )
 
 all_params_short <- c(
-  # "ChlaC",
-  # "Chla",
-  # "CDOM",
-  # "DO",
-  # "DOS",
-  # "pH",
-  # "Sal",
-  # "Secchi",
-  # "TN",
-  # "TP",
-  # "TSS",
+  "ChlaC",
+  "Chla",
+  "CDOM",
+  "DO",
+  "DOS",
+  "pH",
+  "Sal",
+  "Secchi",
+  "TN",
+  "TP",
+  "TSS",
   "Turb",
   "TempW"
 )
 
-table_types <- c("KendallTau_Stats","MA_MMYY_Stats","MA_Mo_Stats","MA_Yr_Stats","MonLoc_Stats","VQSummary")
+table_types <- c("MA_MMYY_Stats","MA_Mo_Stats","MA_Yr_Stats","MonLoc_Stats","VQSummary")
 
 #### Function to create unified data tables ####
 
@@ -65,9 +66,21 @@ create_data_table <- function(table) {
   print(paste0("Wrote ", table, ".xlsx to file"))
 }
 
+## Start script
+tic()
+Start_time <- Sys.time()
+
 print("#### Starting Export ####")
+
 for (t in table_types) {
   print(t)
   create_data_table(t)
 }
+
 print("#### Data Table Exports Complete ####")
+
+toc()
+End_time <- Sys.time()
+
+print(Start_time)
+print(End_time)
