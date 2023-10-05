6e83b7b4bc2ba09f6cc85433a6485ea03b000dc7
tylerhill122 <tylergenehill122@gmail.com>
2023-10-05 09:52:04 -0400

    init commit, creating reports for each ManagedArea

---
+++ MA Report Generation/ReportRender.R
@@ -0,0 +1,118 @@
+#Load libraries
+library(knitr)
+library(readr)
+library(tidyverse)
+library(data.table)
+library(dplyr)
+library(purrr)
+library(rstudioapi)
+library(stringr)
+library(utils)
+
+# Gets directory of this script and sets it as the working directory
+wd <- dirname(getActiveDocumentContext()$path)
+setwd(wd)
+
+# Create folder paths if they don't yet exist
+folder_paths <- c("output", "output/Figures","output/Figures/BB","output/Reports", 
+  "output/Data", "output/models", "output/tables","output/Data/Nekton", "output/Data/SAV",
+  "output/Data/Coral", "output/Data/Coral/PercentCover", "output/Data/Coral/SpeciesRichness",
+  "output/tables", "output/tables/disc", "output/tables/cont", "output/tables/SAV", "output/Data/CoastalWetlands")
+for (path in folder_paths){if(!dir.exists(path)){dir.create(path)}}
+
+#Set output directory
+out_dir <- "output"
+report_out_dir <- "output/Reports"
+
+#Loads data file with list on managed area names and corresponding area IDs and short names
+MA_All <- fread("data/ManagedArea.csv", sep = ",", header = TRUE, stringsAsFactors = FALSE,
+                na.strings = "")
+
+#Gets the desired file locations
+files <- list.files("data", full=TRUE)
+files <- str_subset(files, "All_")
+
+## file names ##
+cw_file_in <- list.files("data", pattern="All_CW", full=TRUE)
+cw_file_short <- sub("data/", "", cw_file_in)
+
+coral_file_in <- list.files("data", pattern="All_CORAL", full=TRUE)
+coral_file_short <- sub("data/", "", coral_file_in)
+
+nekton_file_in <- list.files("data", pattern="All_NEKTON", full=TRUE)
+nekton_file_short <- sub("data/", "", nekton_file_in)
+
+sav_file_in <- list.files("data", pattern="All_SAV", full=TRUE)
+sav_file_short <- sub("data/", "", sav_file_in)
+
+wq_discrete_file <- fread("output/tables/disc/disc_file_list.txt", sep='|')
+wq_discrete_files <- wq_discrete_file %>% 
+  pivot_longer(cols=names(wq_discrete_file)) %>%
+  pull(unique(value))
+
+wq_cont_file <- fread("output/tables/cont/cont_file_list.txt", sep='|')
+wq_cont_files <- wq_cont_file %>%
+  pivot_longer(cols=names(wq_cont_file)) %>%
+  pull(unique(value))
+
+############################
+### call in source files ###
+############################
+# creates source files (.rds objects) for discrete WQ
+# source("WQ_Discrete_Data_Creation.R")
+# source("WQ_Continuous_Data_creation.R")
+source("WQ_Continuous.R")
+source("WQ_Discrete.R")
+source("Nekton.R")
+source("CoastalWetlands.R")
+# creates source files (.rds objects) for SAV
+# source("SAV.R")
+source("SAV-Functions.R")
+source("Coral.R")
+############################
+
+# Subset for MAs
+# MA_All <- MA_All[c(20,14,5,24,32,27,9,33)]
+MA_All <- MA_All[14]
+
+# iterate through every possible MA
+# apply checks for coral, sav, etc. within .Rmd doc
+for (i in seq_len(nrow(MA_All))) {
+  
+  ma <- MA_All[i, ]$ManagedAreaName
+  ma_short <- MA_All[i, ]$ShortName
+  
+  # MA abbreviation
+  ma_abrev <- gsub("[^::A-Z::]","", ma)
+  
+  # perform checks for habitats in each MA
+  # Check which habitats to include in each MA
+  in_sav <- ma_abrev %in% sav_managed_areas
+  in_nekton <- ma %in% nekton_managed_areas
+  in_coral <- ma %in% coral_managed_areas
+  in_cw <- ma %in% cw_managed_areas
+  in_discrete <- ma %in% disc_managed_areas
+  # in_continuous <- ma %in% cont_managed_areas
+
+  #####################
+  ### RENDER REPORT ###
+  #####################
+  
+  if(in_sav | in_nekton | in_coral | in_cw | in_discrete){
+    
+    ma_report_out_dir <- paste0(report_out_dir, "/", ma_abrev)
+    
+    file_out <-  paste0(ma_abrev, "_Report")
+    
+    rmarkdown::render(input = "ReportTemplate.Rmd", 
+                      output_format = "pdf_document",
+                      output_file = paste0(file_out, ".pdf"),
+                      output_dir = ma_report_out_dir,
+                      clean=TRUE)
+    
+    #Removes unwanted files created in the rendering process
+    unlink(paste0(ma_report_out_dir, "/", file_out, ".md"))
+    unlink(paste0(ma_report_out_dir, "/", file_out, "_files"), recursive=TRUE)
+    
+  }
+}
