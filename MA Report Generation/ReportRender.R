b0595b8c9a34a27c5ea435bb2e348245a64b8265
tylerhill122 <tylergenehill122@gmail.com>
2023-12-13 10:22:35 -0500

    updated for new filepath designation, file name fixes, new exports Dec08

--- MA Report Generation/ReportRender.R
+++ MA Report Generation/ReportRender.R
@@ -3,7 +3,6 @@ library(knitr)
 library(readr)
 library(tidyverse)
 library(data.table)
-library(dplyr)
 library(purrr)
 library(rstudioapi)
 library(stringr)
@@ -21,6 +20,8 @@ library(gridExtra)
 library(ggpubr)
 library(glue)
 library(kableExtra)
+library(distill)
+library(dplyr)
 
 # Gets directory of this script and sets it as the working directory
 wd <- dirname(getActiveDocumentContext()$path)
@@ -31,7 +32,7 @@ folder_paths <- c("output", "output/Figures","output/Figures/BB","output/Reports
   "output/Data", "output/models", "output/tables","output/Data/Nekton", "output/Data/SAV",
   "output/Data/Coral", "output/Data/Coral/PercentCover", "output/Data/Coral/SpeciesRichness",
   "output/tables", "output/tables/disc", "output/tables/cont", "output/tables/SAV", "output/Data/CoastalWetlands",
-  "output/maps", "output/Figures/BB/maps")
+  "output/maps","output/maps/discrete","output/Figures/BB/maps", "output/Density", "output/Shell_Height", "output/Percent_Live")
 for (path in folder_paths){if(!dir.exists(path)){dir.create(path)}}
 
 #Set output directory
@@ -43,42 +44,47 @@ MA_All <- fread("data/ManagedArea.csv", sep = ",", header = TRUE, stringsAsFacto
                 na.strings = "")
 
 #Gets the desired file locations
-files <- list.files("data", full=TRUE)
+#Imports SEACAR data file path information as variable "seacar_data_location"
+source("scripts/SEACAR_data_location.R")
+
+files <- list.files(here::here(seacar_data_location), full=TRUE)
 files <- str_subset(files, "All_")
 
-cw_file_in <- list.files("data", pattern="All_CW", full=TRUE)
-cw_file_short <- sub("data/", "", cw_file_in)
+cw_file_in <- str_subset(files, "CW")
+cw_file_short <- tail(str_split(cw_file_in, "/")[[1]],1)
 
-coral_file_in <- list.files("data", pattern="All_CORAL", full=TRUE)
-coral_file_short <- sub("data/", "", coral_file_in)
+coral_file_in <- str_subset(files, "CORAL")
+coral_file_short <- tail(str_split(coral_file_in, "/")[[1]],1)
 
-nekton_file_in <- list.files("data", pattern="All_NEKTON", full=TRUE)
-nekton_file_short <- sub("data/", "", nekton_file_in)
+nekton_file_in <- str_subset(files, "NEKTON")
+nekton_file_short <- tail(str_split(nekton_file_in, "/")[[1]],1)
 
-sav_file_in <- list.files("data", pattern="All_SAV", full=TRUE)
-sav_file_short <- sub("data/", "", sav_file_in)
+sav_file_in <- str_subset(files, "SAV")
+sav_file_short <- tail(str_split(sav_file_in, "/")[[1]],1)
+
+oyster_file_in <- str_subset(files, "Oyster")
+oyster_file_short <- tail(str_split(oyster_file_in, "/")[[1]],1)
 
 ############################
 ### call in source files ### -----
 ############################
-# creates source files (.rds objects) for discrete WQ
-# source("scripts/WQ_Discrete_Data_Creation.R")
-# source("scripts/WQ_Continuous_Data_creation.R")
+
+source("scripts/load_shape_files.R")
+# source("scripts/WQ_Discrete_Data_Creation.R") # creates source files (.rds objects) for discrete WQ
+# source("scripts/WQ_Continuous_Data_creation.R") # creates source files (.rds objects) for continuous WQ
 source("scripts/WQ_Continuous.R")
 source("scripts/WQ_Discrete.R")
 source("scripts/Nekton.R")
 source("scripts/CoastalWetlands.R")
-# creates source files (.rds objects) for SAV
-# source("scripts/SAV.R")
-# source("scripts/SAV_scope_plots.R")
+source("scripts/SAV.R") # creates source files (.rds objects) for SAV
+source("scripts/SAV_scope_plots.R")
 source("scripts/SAV-Functions.R")
 source("scripts/Coral.R")
 ############################
 
 ################
 ## file names ##
-################
-
+# Pulls file names from discrete and cont. file list .txt rendered during .RDS object creation
 wq_discrete_file <- fread("output/tables/disc/disc_file_list.txt", sep='|')
 wq_discrete_files <- wq_discrete_file %>% 
   pivot_longer(cols=names(wq_discrete_file)) %>%
@@ -88,13 +94,15 @@ wq_cont_file <- fread("output/tables/cont/cont_file_list.txt", sep='|')
 wq_cont_files <- wq_cont_file %>%
   pivot_longer(cols=names(wq_cont_file)) %>%
   pull(unique(value))
+# Removes file path and isolates file names
+wq_cont_files_short <- lapply(wq_cont_files, function(x){tail(str_split(x, "/")[[1]],1)})
 
 #################
 #################
 
 # Subset for MAs
 # MA_All <- MA_All[c(14,5,32,27,9,33)]
-MA_All <- MA_All[c(14,5,32)]
+MA_All <- MA_All[c(14)]
 
 # iterate through every possible MA
 # apply checks for coral, sav, etc. within .Rmd doc
